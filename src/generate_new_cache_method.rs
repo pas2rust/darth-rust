d614ba8219f942c5e954cb2135b33b33520b8e08
pas2rust <145881352+pas2rust@users.noreply.github.com>
2023-11-05 18:12:52 -0300

    up

---
+++ src/generate_new_cache_method.rs
@@ -0,0 +1,14 @@
+use quote::quote;
+use syn::{Data, Fields, Ident};
+use chrono::DateTime;
+
+pub struct Cache<T, D> {
+    pub items: Vec<T>,
+    pub threads: u64,
+    pub max_ram: f64,
+    pub expiration: DateTime<D>
+}
+
+pub fn generate_new_cache_method(fields: &Data) -> proc_macro2::TokenStream {
+    
+}
--- src/tools/darth_tools.rs
+++ src/tools/darth_tools.rs
@@ -1,9 +1,41 @@
 use crate::tools::random_bytes::random_bytes;
-use chrono::{DateTime, Utc, Local, Duration};
+use chrono::{DateTime, Duration, Local, Utc};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use uuid::Uuid;
 pub struct DarthTools;
 
 pub trait DarthToolsTrait {
+    fn new_jsonwebtoken_hs256_decode(
+        token: String,
+        secret: String,
+    ) -> Result<Token, jsonwebtoken::errors::Error>;
+    fn new_jsonwebtoken_hs384_decode(
+        token: String,
+        secret: String,
+    ) -> Result<Token, jsonwebtoken::errors::Error>;
+    fn new_jsonwebtoken_hs512_decode(
+        token: String,
+        secret: String,
+    ) -> Result<Token, jsonwebtoken::errors::Error>;
+    fn new_jsonwebtoken_hs256_encode(
+        items: Value,
+        expiration: usize,
+        secret: String,
+    ) -> Result<String, jsonwebtoken::errors::Error>;
+    fn new_jsonwebtoken_hs384_encode(
+        items: Value,
+        expiration: usize,
+        secret: String,
+    ) -> Result<String, jsonwebtoken::errors::Error>;
+    fn new_jsonwebtoken_hs512_encode(
+        items: Value,
+        expiration: usize,
+        secret: String,
+    ) -> Result<String, jsonwebtoken::errors::Error>;
+    fn new_bcrypt_hash(value: String, cost: u32) -> Result<String, bcrypt::BcryptError>;
+    fn new_bcrypt_verify(value: String, hash: &String) -> Result<bool, bcrypt::BcryptError>;
     fn new_date_utc_add_time_by_weeks(weeks: i64) -> DateTime<Utc>;
     fn new_date_utc_add_time_by_days(days: i64) -> DateTime<Utc>;
     fn new_date_utc_add_time_by_hours(hours: i64) -> DateTime<Utc>;
@@ -26,7 +58,91 @@ pub trait DarthToolsTrait {
     ) -> String;
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct Token {
+    pub items: Value,
+    pub expiration: usize,
+}
+
 impl DarthToolsTrait for DarthTools {
+    fn new_jsonwebtoken_hs256_decode(
+            token: String,
+            secret: String,
+        ) -> Result<Token, jsonwebtoken::errors::Error> {
+            let res = decode::<Token>(
+                &token,
+                &DecodingKey::from_secret(secret.as_ref()),
+                &Validation::new(jsonwebtoken::Algorithm::HS256),
+            )?;
+            Ok(res.claims)
+    }
+    fn new_jsonwebtoken_hs384_decode(
+            token: String,
+            secret: String,
+        ) -> Result<Token, jsonwebtoken::errors::Error> {
+            let res = decode::<Token>(
+                &token,
+                &DecodingKey::from_secret(secret.as_ref()),
+                &Validation::new(jsonwebtoken::Algorithm::HS384),
+            )?;
+            Ok(res.claims)
+    }
+    fn new_jsonwebtoken_hs512_decode(
+        token: String,
+        secret: String,
+    ) -> Result<Token, jsonwebtoken::errors::Error> {
+        let res = decode::<Token>(
+            &token,
+            &DecodingKey::from_secret(secret.as_ref()),
+            &Validation::new(jsonwebtoken::Algorithm::HS512),
+        )?;
+        Ok(res.claims)
+    }
+    fn new_jsonwebtoken_hs256_encode(
+        items: Value,
+        expiration: usize,
+        secret: String,
+    ) -> Result<String, jsonwebtoken::errors::Error> {
+        let my_claims = Token { items, expiration };
+        let header = Header::new(jsonwebtoken::Algorithm::HS256);
+        encode(
+            &header,
+            &my_claims,
+            &EncodingKey::from_secret(secret.as_ref()),
+        )
+    }
+    fn new_jsonwebtoken_hs384_encode(
+        items: Value,
+        expiration: usize,
+        secret: String,
+    ) -> Result<String, jsonwebtoken::errors::Error> {
+        let my_claims = Token { items, expiration };
+        let header = Header::new(jsonwebtoken::Algorithm::HS384);
+        encode(
+            &header,
+            &my_claims,
+            &EncodingKey::from_secret(secret.as_ref()),
+        )
+    }
+    fn new_jsonwebtoken_hs512_encode(
+        items: Value,
+        expiration: usize,
+        secret: String,
+    ) -> Result<String, jsonwebtoken::errors::Error> {
+        let my_claims = Token { items, expiration };
+        let header = Header::new(jsonwebtoken::Algorithm::HS512);
+        encode(
+            &header,
+            &my_claims,
+            &EncodingKey::from_secret(secret.as_ref()),
+        )
+    }
+    fn new_bcrypt_hash(value: String, cost: u32) -> Result<String, bcrypt::BcryptError> {
+        bcrypt::hash(value, cost)
+    }
+    fn new_bcrypt_verify(value: String, hash: &String) -> Result<bool, bcrypt::BcryptError> {
+        bcrypt::verify(value, hash)
+    }
     fn new_date_utc_add_time_by_hours(hours: i64) -> DateTime<Utc> {
         let now = Utc::now();
         let duration = Duration::hours(hours);
@@ -84,15 +200,15 @@ impl DarthToolsTrait for DarthTools {
         Utc::now()
     }
     fn new_uuid() -> String {
-       Uuid::new_v4().to_string()
+        Uuid::new_v4().to_string()
     }
     fn new_random_bytes(
-            gen_uppercase: Option<u32>,
-            gen_lowercase: Option<u32>,
-            gen_number: Option<u32>,
-            gen_special_characters: Option<u32>,
-            gen_emoji: Option<u32>,
-        ) -> String {
+        gen_uppercase: Option<u32>,
+        gen_lowercase: Option<u32>,
+        gen_number: Option<u32>,
+        gen_special_characters: Option<u32>,
+        gen_emoji: Option<u32>,
+    ) -> String {
         random_bytes(
             gen_uppercase,
             gen_lowercase,
