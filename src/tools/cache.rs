de6c18f5617f87ccd5e93c4ea59a02ceb880b22a
pas2rust <145881352+pas2rust@users.noreply.github.com>
2023-11-23 21:52:04 -0300

    0.2.4

--- src/tools/cache.rs
+++
@@ -1,9 +0,0 @@
-use chrono::{DateTime, Utc};
-
-pub struct Cache<T> {
-    pub items: Vec<T>,
-    pub threads: u64,
-    pub max_ram: f64,
-    pub expiration: DateTime<Utc>,
-    pub requests: u64
-}
