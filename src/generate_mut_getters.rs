bcb3eab073390ecc86aa2e64cae3aad35096f03b
pas2rust <145881352+pas2rust@users.noreply.github.com>
2024-01-20 08:48:09 -0300

    0.4.7

--- src/generate_mut_getters.rs
+++ src/generate_mut_getters.rs
@@ -14,30 +14,6 @@ pub fn generate_mut_getters(fields: &Data) -> proc_macro2::TokenStream {
                     );
 
                     quote! {
-                        /// Get a mutable reference to the `{field_name}` field of the struct.
-                        ///
-                        /// This method allows you to obtain a mutable reference to the `{field_name}`
-                        /// field of the struct, which you can use to modify its value.
-                        ///
-                        /// # Example
-                        ///
-                        ///```rust
-                        /// use darth_rust::DarthRust;
-                        ///
-                        /// #[derive(DarthRust, Debug, PartialEq)]
-                        /// struct MyStruct {
-                        ///     field1: String,
-                        ///     field2: i32,
-                        /// }
-                        ///
-                        /// let new_instance = MyStruct::new("Hello".to_string(), 18);
-                        /// *new_instance.get_mut_field1() = "mut hello".to_string();
-                        ///
-                        /// assert_eq!(new_instance.field1, "mut hello");
-                        /// ```
-                        /// # Returns
-                        ///
-                        /// A mutable reference to the `{field_name}` field.
                         pub fn #method_name(&mut self) -> &mut #field_type {
                             &mut self.#field_name
                         }
