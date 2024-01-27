a4b9d514a7c662eb3ddbb8623b577e63a9f8e024
pas2rust <145881352+pas2rust@users.noreply.github.com>
2024-01-27 08:45:11 -0300

    0.5.1

--- src/generate_new_method.rs
+++ src/generate_new_method.rs
@@ -1,5 +1,5 @@
 use quote::quote;
-use syn::{Data, Generics, Type};
+use syn::{Data, Generics};
 
 pub fn generate_new_method(
     data: &Data,
@@ -29,12 +29,11 @@ pub fn generate_new_method(
                 }
             });
 
-            let generics = generics.clone();
             let (impl_generics, ty_generics, where_clause) =
                 generics.split_for_impl();
 
             quote! {
-                pub fn new #impl_generics (#(#field_param_initializers),*) -> Self #where_clause {
+                pub fn new #impl_generics (#(#field_param_initializers),*) -> Self #ty_generics #where_clause {
                     Self {
                         #(#field_names)*
                     }
